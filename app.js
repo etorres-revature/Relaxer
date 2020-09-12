a93000330a5ce025b55af4a6d6e784ec79d82abe
Eric D. Torres <etorresnotary@gmail.com>
2020-09-11 20:22:10 -0500

    ubtton to play and pause relaxing music

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //global variables to grab DOM elements
 const containerEl = document.querySelector("#container");
 const textEl = document.querySelector("#text");
-
+const audioEl = document.querySelector("audio")
+// const iFrameEl = document.querySelector("iframe")
+const playBtn = document.querySelector("#play-btn")
 const totalTime = 7500;
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
@@ -24,4 +26,28 @@ function breathAnimation() {
   }, breathTime);
 }
 
+function play() {
+  if (audioEl.paused) {
+    audioEl.play()
+  } else {
+    audioEl.pause();
+  }
+}
+
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+//   if (!isChrome){
+//       iframeEl.remove()
+//   }
+//   else {
+//       audioEl.remove() // just to make sure that it will not have 2x audio in the background 
+//   }
+
+// window.onload = function () {
+//   audioEl.play();
+// }
+
 setInterval(breathAnimation, totalTime);
+
+playBtn.addEventListener("click", play);
+
+
